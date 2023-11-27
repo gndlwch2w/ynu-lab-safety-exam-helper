444d4b78a6ab8ef849c583997fc89dd15ba93b10
Chisheng Chen <139485529+gndlwch2w@users.noreply.github.com>
2023-11-27 10:54:01 +0800

    Update and rename helper_v2.js to helper_v1.0.0.js

--- helper_v1.0.0.js
+++ helper_v1.0.0.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name     YNU Lab Safety Exam Helper v2
+// @name     YNU Lab Safety Exam Helper v1.0.0
 // @include  https://learn-sysysb.ynu.edu.cn/learn/*
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @require  https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js
