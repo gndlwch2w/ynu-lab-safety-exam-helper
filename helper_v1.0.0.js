1f5240d9ed737aba80a41b231c7ad3eaa774699c
Chisheng Chen <139485529+gndlwch2w@users.noreply.github.com>
2023-11-27 10:54:11 +0800

    Update helper_v1.0.0.js

--- helper_v1.0.0.js
+++ helper_v1.0.0.js
@@ -4,7 +4,7 @@
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @require  https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js
 // @author   Chisheng Chen
-// @version  0.0.2
+// @version  1.0.0
 // @license  MIT
 // @grant    none
 // ==/UserScript==
